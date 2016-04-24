e202dde38d915a1aa83f40d46095d10184ea6103
lw <cui@hellolw.com>
2016-04-24 11:42:37 +0800

    add copy and assignment test

--- test.cpp
+++ test.cpp
@@ -7,14 +7,15 @@
 namespace {
 	const int MAX = 1000000;
 	const int REPEATE = 10000;
-	class thread_guard {
+	class Scope_thread {
 	public:
-		explicit thread_guard(std::thread &t):mythread{t}{}
-		~thread_guard() {if (mythread.joinable()) mythread.join();}
-		thread_guard(const thread_guard &)=delete;
-		thread_guard& operator=(const std::thread &)=delete;
+		// Only accept rvalue reference
+		explicit Scope_thread(std::thread t):mythread{std::move(t)}{}
+		~Scope_thread() {mythread.join();}
+		Scope_thread(const Scope_thread&)=delete;
+		Scope_thread& operator=(const Scope_thread)=delete;
 	private:
-		std::thread &mythread;
+		std::thread mythread;
 	};
 }
 
@@ -28,9 +29,43 @@ TEST(BasicTest, InsertAndDel) {
 		int tmp{rand()};
 		pq.push(tmp); spq.push(tmp);
 	}
+	while (spq.size() > REPEATE / 2) {
+		ASSERT_EQ(spq.top(), pq.top());
+		spq.pop(); pq.pop();
+	}
+	for (int i = 0; i < REPEATE; i++) {
+		int tmp{rand()};
+		pq.push(tmp); spq.push(tmp);
+		spq.pop(); pq.pop();
+	}
 	while (!spq.empty()) {
 		ASSERT_EQ(spq.top(), pq.top());
 		spq.pop(); pq.pop();
 	}
 }
 
+TEST(BasicTest, CopyAndAssign) {
+	que::PriorityQueue<int> pq;
+	std::vector<int> v;
+	for (auto i = 0; i < REPEATE; i++) {
+		pq.push(i); v.push_back(i);
+	}
+	que::PriorityQueue<int> copyed{pq};
+	que::PriorityQueue<int> assigned; assigned = pq;
+	que::PriorityQueue<int> rvalue_copyed
+		{que::PriorityQueue<int>{v.begin(), v.end()}};
+	que::PriorityQueue<int> rvalue_assigned 
+		= que::PriorityQueue<int>{v.begin(), v.end()};
+	while (!copyed.empty()) {
+		ASSERT_EQ(copyed.top(), assigned.top());
+		ASSERT_EQ(copyed.top(), rvalue_copyed.top());
+		ASSERT_EQ(copyed.top(), rvalue_assigned.top());
+		copyed.pop(); assigned.pop(); 
+		rvalue_copyed.pop(); rvalue_assigned.pop();
+	}
+}
+
+TEST(ThreadTest, Insert) {
+	que::PriorityQueue<int> pq;
+	Scope_thread p{std::thread{[&](){pq.push(10);}}};
+}
